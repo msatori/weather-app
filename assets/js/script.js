714435cc691ef0e25e7ccaa69692b3d7b41c2ba1
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-11 22:42:14 -0500

    added forecast and search history save element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,123 +1,182 @@
 //our html will be structured in a way that allows for containers 
-var cityInputEl = document.getElementById("#search-input");
-var submitButtonEl = document.getElementById("#search-butt")
-var cityName = `Madison`
 const dateTime = moment().format(" dddd, MMMM Do YYYY, h:mm a");
 var todayEl = document.getElementById("#today");
-
 //document.querySelector("#search-input").value;
 
 
-//access openweathermap + get a response
+
 //format the openweathermap api
 
 
-$("#submit-butt").on("click", function () {
-var citySubmitHandler = function (event) {
+
+function citySubmitHandler() {
+    var searchInput = document.querySelector("#search-input").value;
+    getWeather(searchInput)
+    saveHistory(searchInput);
     
-        event.preventDefault();
-        var citySearchInput = submitButtonEl.val().trim();
-        if (citySearchInput) {
-            console.log(citySearchInput);
-        }
-        else {
-            alert("Please Enter a City")
-        }
-        console.log(event)
-
-    };
-    citySubmitHandler();
-});
+};
 
 
-//create a variable to display city
-var displayCityWeather = function (weather) {
-    //check if api found cities weather 
-
-}
-
-
-function getWeather() {
+//access openweathermap + get a response
+function getWeather(searchInput) {
 
     //use the fetch function to retreive an open weather api response
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8f7b4d33732f41b547ee00737e827bb7&units=imperial`
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=8f7b4d33732f41b547ee00737e827bb7&units=imperial"
     fetch(apiURL).then(function (response) {
         response.json().then(function (data) {
             console.log(data);
             console.log(data.weather[0].description);
-       
-        //.then(function (data) {
-        //clear old content
-        todayEl = document.querySelector("#today");
-        todayEl.textContent = "";
-        
-        //create HTML elements for todays weather (heading, div, p)
-        //add html elements (classes, cards for forecast and today)
+
+            //.then(function (data) {
+            //clear old content
+            todayEl = document.querySelector("#today");
+            todayEl.textContent = "";
+
+            //create HTML elements for todays weather (heading, div, p)
+            //add html elements (classes, cards for forecast and today)
 
 
 
-        //create a title element for today's forecast
-        var titleEl = document.createElement("h3");
-        titleEl.classList.add("card-title");
-        titleEl.textContent = data.name + "," + dateTime;
-    
-        
-        //create a card for storing today's forecast
-        var cardEl = document.createElement("div");
-        cardEl.classList.add("card");
+            //create a title element for today's forecast
+            var titleEl = document.createElement("h3");
+            titleEl.classList.add("card-title");
+            titleEl.textContent = data.name + "," + dateTime;
 
-        //make a body for the card
-        var cardBodyEl = document.createElement("div");
-        cardBodyEl.classList.add("card-body");
 
-        //pull data for weather description
-        var weatherDesc = document.createElement("p")
-        weatherDesc.classList.add("card-text");
-        weatherDesc.textContent = data.weather[0].description
+            //create a card for storing today's forecast
+            var cardEl = document.createElement("div");
+            cardEl.classList.add("card");
 
-        //pull data for temperature
-        var tempEl = document.createElement("p");
-        tempEl.classList.add("card-text");
-        tempEl.textContent = "Temperature: " + data.main.temp + "°F";
+            //make a body for the card
+            var cardBodyEl = document.createElement("div");
+            cardBodyEl.classList.add("card-body");
 
-        //pull data for wind speed
-        var windSpeedEl = document.createElement("p");
-        windSpeedEl.classList.add("card-text");
-        windSpeedEl.textContent = "Wind Speed: " + data.wind.speed;
+            //pull data for weather description
+            var weatherDesc = document.createElement("p")
+            weatherDesc.classList.add("card-text");
+            weatherDesc.textContent = data.weather[0].description
 
-        //pull data for humidity
-        var humidEl = document.createElement("p");
-        humidEl.classList.add("p");
-        humidEl.textContent = "Humidity: " + data.main.humidity + "%";
+            //pull data for temperature
+            var tempEl = document.createElement("p");
+            tempEl.classList.add("card-text");
+            tempEl.textContent = "Temperature: " + data.main.temp + "°F";
 
-        //create image element for icons
-        var iconEl = document.createElement("img")
-        iconEl.setAttribute("src", " http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
-        
-        
-      //add icon to title element
-      titleEl.appendChild(iconEl);
-      todayEl.appendChild(titleEl);
-      //place card into today div
-      todayEl.appendChild(cardEl);
-      todayEl.appendChild(cardBodyEl);
-      //place data into card
-      cardBodyEl.appendChild(weatherDesc);
-      cardBodyEl.appendChild(tempEl);
-      cardBodyEl.appendChild(windSpeedEl);
-      cardBodyEl.appendChild(humidEl);
+            //pull data for wind speed
+            var windSpeedEl = document.createElement("p");
+            windSpeedEl.classList.add("card-text");
+            windSpeedEl.textContent = "Wind Speed: " + data.wind.speed + "MPH";
 
+            //pull data for humidity
+            var humidEl = document.createElement("p");
+            humidEl.classList.add("p");
+            humidEl.textContent = "Humidity: " + data.main.humidity + "%";
+
+            //create image element for icons
+            var iconEl = document.createElement("img")
+            iconEl.setAttribute("src", " http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+
+
+            //add icon to title element
+            titleEl.appendChild(iconEl);
+            cardBodyEl.appendChild(titleEl);
+            cardBodyEl.appendChild(weatherDesc);
+            cardBodyEl.appendChild(tempEl);
+            cardBodyEl.appendChild(windSpeedEl);
+            cardBodyEl.appendChild(humidEl);
+            cardEl.appendChild(cardBodyEl);
+            //place card into today div
+            todayEl.appendChild(cardEl);
+           
+
+            getForecast(searchInput);
+
+        });
     });
+};
+
+function getForecast(searchInput) {
+    //fetch forecast API
+    var futureApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&exclude=minutely,hourly&appid=8f7b4d33732f41b547ee00737e827bb7&units=imperial";
+    fetch(futureApi).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        console.log(data);
+        //get forecast container
+        var forecastEl = document.querySelector("#future-forecast");
+        var display = document.createElement("h3");
+        display.classList.add("row");
+        display.textContent = "5 Day Forecast:";
+
+        forecastEl.appendChild(display);
+
+        //loop over the forecasts
+        for (var i = 5; i < 40; i += 8) {
+
+             //display forecast text
+            
+              
+
+                //create cards to push info into
+                var cardColEl = document.createElement("div");
+                cardColEl.classList.add("col-md-2");
+                var cardEl = document.createElement("div");
+                cardEl.classList.add("card", "bg-primary", "text-white");
+                //body for card
+                cardBodyEl = document.createElement("div");
+                cardBodyEl.classList.add("card-body", "p-2");
+                //card deck
+                var cardDeckEl = document.createElement("div");
+                cardDeckEl.classList.add("card-deck", "justify-center");
+
+                //info for date
+                var dateDisplay = document.createElement("h5");
+                dateDisplay.classList.add("card-title");
+                dateDisplay.textContent = data.list[i].dt_hour;
+
+                //pull temperature data
+                var tempEl = document.createElement("p");
+                tempEl.classList.add("card-text");
+                tempEl.textContent = "Temp:" + data.list[i].main.temp + "°F";
+
+                //pull windspeed data
+                var windSpeedEl = document.createElement("p");
+                windSpeedEl.classList.add("card-text")
+                windSpeedEl.textContent = data.list[i].wind.speed + "MPH";
+
+                //pull humidity data
+                var humidEl = document.createElement("p");
+                humidEl.classList.add("card-text");
+                humidEl.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+
+
+                var iconEl = document.createElement("img");
+                iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png")
+
+                //append cards to html and info to cards
+                cardColEl.appendChild(cardEl);
+                
+                cardBodyEl.appendChild(dateDisplay);
+                cardBodyEl.appendChild(iconEl);
+                cardBodyEl.appendChild(tempEl);
+                cardBodyEl.appendChild(windSpeedEl);
+                cardBodyEl.appendChild(humidEl);
+                cardEl.appendChild(cardBodyEl);
+                cardDeckEl.appendChild(cardEl);
+
+                forecastEl.appendChild(cardDeckEl);
+
+            };
+        
     });
+};
+//create search history list--attch to ul element in html
+function saveHistory(searchInput) {
+   let listItem = $("<li>").addClass("list-group-item").text(searchInput);
+   $(".search-history").append(listItem);
     };
 
-function getForecast {
-    
-} 
 
-
-getWeather();
-
+document.querySelector("#search-butt").addEventListener("click", citySubmitHandler);
 
 //based on response, we are going to populate different parts of the html with properties from the response object
 //every time the user enters  search term, we are going to not only create a new button, but also save the search term to local storage--side bar(uses aside in HTML)
